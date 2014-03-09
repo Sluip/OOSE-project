36dd7b76b11a6be099f89f2d8c3c6ed8f08d978a
Sluip <sluip@sp00fed.dk>
2014-03-09 19:19:22 +0100

    Character (Left Right) movement working

---
+++ Assets/Scripts/CharMovement3.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharMovement3 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	public float maxMovementSpeed = 10.0f;
+	bool right = true;
+	// Update is called once per frame
+	void FixedUpdate () {
+		float move = Input.GetAxis("Horizontal");
+		rigidbody2D.velocity = new Vector2(move * maxMovementSpeed, rigidbody2D.velocity.y);
+		if (move > 0 && !right){
+			Flip();
+		}
+		else if (move < 0 && right){
+			Flip();
+		}
+
+	}
+
+	void Flip(){
+		right = !right;
+		Vector3 scale = transform.localScale;
+		scale.x *= -1;
+		transform.localScale = scale;
+
+	
+}
+}
