400ec28cafd72d86f403ee3be42ae9863ee21fde
Sluip <sluip@sp00fed.dk>
2014-03-09 21:20:57 +0100

    Jumping Script fix

--- Assets/Scripts/JumpingScript.cs
+++ Assets/Scripts/JumpingScript.cs
@@ -16,9 +16,6 @@ public class JumpingScript : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () {
 		grounded = Physics2D.OverlapCircle(groundChecker.position, checkerRadius, consideredGround);
-	
-	}
-	void Update () {
 		if (grounded && Input.GetButton ("Jump")) {
 			rigidbody2D.AddForce(new Vector2(0, jumpSpeed));
 		}
