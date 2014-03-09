d5e0fff62729f2a9417281586fb9d86ad1f86999
mbaatrup <mbaatrup@gmail.com>
2014-03-09 19:20:17 +0100

    Fighting script added

---
+++ Assets/Scripts/FightingScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FightingScript : MonoBehaviour {
+
+	public float speed;
+	public float range;
+	public Transform player;
+	public CharacterController controller;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Debug.Log (inRange());
+	
+	}
+
+	bool inRange()
+	{
+		if(Vector2.Distance(transform.position, player.position)<range)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+}
