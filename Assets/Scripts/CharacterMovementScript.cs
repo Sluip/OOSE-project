4ff2cbe939dc9f5b551c1ad4d7eb0fc0678c4c17
mbaatrup <mbaatrup@gmail.com>
2014-03-10 12:13:57 +0100

    attack and health script deleted. charmovement3 renamed to CharacterMovementScript

--- Assets/Scripts/CharacterMovementScript.cs
+++ Assets/Scripts/CharacterMovementScript.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CharMovement3 : MonoBehaviour {
+public class CharacterMovementScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
