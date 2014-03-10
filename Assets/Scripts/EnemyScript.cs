5d95964caf994dcfd45ff8068a095eeac4d292f0
mbaatrup <mbaatrup@gmail.com>
2014-03-10 22:30:06 +0100

    PlayerScript and EnemyScript updated

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using System.Collections;
+
+public class EnemyScript : MonoBehaviour {
+
+	public int HP = 10;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(HP <= 0)
+		{
+			Destroy(gameObject);
+		}
+	
+	}
+
+	public void Hurt(int damage)
+	{
+		HP -= damage;
+		Debug.Log (HP);
+	}
+}
