c7e20a85bf5cf3960e99c3bcf90bd733aa9ad32c
Sluip <sluip@sp00fed.dk>
2014-03-11 01:19:03 +0100

    Movement stickyness fixed, WIP on air movement

--- Assets/Scripts/CharacterMovementScript.cs
+++ Assets/Scripts/CharacterMovementScript.cs
@@ -1,33 +1,56 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
-public class CharacterMovementScript : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	public float maxMovementSpeed = 10.0f;
-	bool right = true;
-	// Update is called once per frame
-	void FixedUpdate () {
-		float move = Input.GetAxis("Horizontal");
-		rigidbody2D.velocity = new Vector2(move * maxMovementSpeed, rigidbody2D.velocity.y);
-		if (move > 0 && !right){
-			Flip();
-		}
-		else if (move < 0 && right){
-			Flip();
+public class CharacterMovementScript : MonoBehaviour
+{
+		public JumpingScript jumpScript;
+		public float maxMovementSpeed = 10.0f;
+		bool right = true;
+		bool airStop;
+		// Use this for initialization
+		void Start ()
+		{
+				jumpScript = GetComponent<JumpingScript> ();
+				airStop = false;
 		}
 
-	}
+		// Update is called once per frame
+		void FixedUpdate ()
+		{
+		if (jumpScript.Grounded ()) {
+						airStop = false;
+				}
+				if (!airStop) {
+						float move = Input.GetAxis ("Horizontal");
+						rigidbody2D.velocity = new Vector2 (move * maxMovementSpeed, rigidbody2D.velocity.y);
+				
+						if (move > 0 && !right) {
+								Flip ();
+						} else if (move < 0 && right) {
+								Flip ();
+						}
+		}
 
-	void Flip(){
-		right = !right;
-		Vector3 scale = transform.localScale;
-		scale.x *= -1;
-		transform.localScale = scale;
+				}
+		
+
+		void Flip ()
+		{
+				right = !right;
+				Vector3 scale = transform.localScale;
+				scale.x *= -1;
+				transform.localScale = scale;
 
 	
-}
+		}
+
+		void OnCollisionStay2D (Collision2D coll)
+		{
+				if (coll.gameObject.tag == "Ground" && !jumpScript.Grounded ()) {
+						if (Mathf.Abs (rigidbody2D.velocity.x) <= 0.0f) {
+								airStop = true;
+						}
+				}
+
+		}
 }
