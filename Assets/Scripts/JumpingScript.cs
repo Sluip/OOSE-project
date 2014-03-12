35d4a925c2958f874f84fb9b6076092e272bf077
mbaatrup <mbaatrup@gmail.com>
2014-03-12 10:08:53 +0100

    New jumping system implemented(JumpingScript)

--- Assets/Scripts/JumpingScript.cs
+++ Assets/Scripts/JumpingScript.cs
@@ -8,16 +8,57 @@ public class JumpingScript : MonoBehaviour {
 	public Transform groundChecker;
 	public LayerMask consideredGround;
 	float checkerRadius = 0.2f;
-	public float jumpSpeed = 20.0f;
+	[HideInInspector] public float jumpSpeed = 20.0f;
+	private bool isJumping = false;
+	private bool isCharging = false;
+	public float startJumpSpeed = 500.0f;
+	public float maxJumpSpeed = 1000.0f;
+	public float jumpSpeedMultiplier = 7.0f;
+
 	void Start () {
 	
+	}
+
+	void Update()
+	{
+		// Jump when space is released
+		if(Input.GetButtonUp ("Jump"))
+		{
+			isJumping = true;
+			isCharging = false;
+
+			if(jumpSpeed > maxJumpSpeed)
+			{
+				Debug.Log(maxJumpSpeed);
+				jumpSpeed = maxJumpSpeed;
+			}
+		}
+
+		// Jump is charging while space is pressed
+		if(Input.GetButton ("Jump"))
+		{
+			isCharging = true;
+			// TODO set running speed--
+		}
+
+		// Jump speed is set to default value when space is pressed down
+		if(Input.GetButtonDown ("Jump"))
+		{
+			jumpSpeed = startJumpSpeed;
+		}
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
 		grounded = Physics2D.OverlapCircle(groundChecker.position, checkerRadius, consideredGround);
-		if (grounded && Input.GetButton ("Jump")) {
+		if (grounded && isJumping) {
 			rigidbody2D.AddForce(new Vector2(0, jumpSpeed));
+			isJumping = false;
+		}
+		if(grounded && isCharging)
+		{
+			Debug.Log(jumpSpeed);
+			jumpSpeed += jumpSpeedMultiplier;
 		}
 	}
 	public bool Grounded() {
