a5470ebd8dcb03a30e18484053e1be9987ea1531
Sluip <sluip@sp00fed.dk>
2014-03-16 15:46:44 +0100

    Camera fixed to follow player correctly

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -5,6 +5,8 @@ public class CameraScript : MonoBehaviour {
 	public float moveSpeed = 0.20f;
 	private Vector3 v = Vector3.zero;
 	public Transform player;
+	private Vector3 endpoint;
+	private bool topScreen = false;
 	// Use this for initialization
 	void Start () {
 	
@@ -13,16 +15,25 @@ public class CameraScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (player) {
+
 						Vector3 point = camera.WorldToViewportPoint (player.position);
 						Vector3 delta = player.position - camera.ViewportToWorldPoint (new Vector3 (0.5f, 0.5f, point.z));
-						Vector3 endpoint = transform.position + delta;
+						endpoint = transform.position + delta;
 			if (point.y > 0.7f) {
-				endpoint.y = 0.0f;
+				endpoint.y = endpoint.y;
+				topScreen = true;
+			}
+			else if (player.position.y < 1.0f) {
+				topScreen = false;
+			}
 
 			}
-		
-						transform.position = Vector3.SmoothDamp (transform.position, endpoint, ref v, moveSpeed);
-				}
+			if (!topScreen) {
+				endpoint.y = 0.0f;
+			}
+		Debug.Log(topScreen);
+		transform.position = Vector3.SmoothDamp (transform.position, endpoint, ref v, moveSpeed);		
+		}
 	
 	}
-}
+
