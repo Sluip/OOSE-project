41c817b83023de415ad4338eaa757f96c940f14d
mbaatrup <mbaatrup@gmail.com>
2014-04-30 15:02:38 +0200

    WinScreen

---
+++ Assets/WinScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WinScript : MonoBehaviour {
+
+	public GameObject winScreen;
+
+	// Use this for initialization
+	void Start () {
+	
+		winScreen.renderer.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.gameObject.tag == "Player")
+		{
+			Destroy (other.gameObject);
+			winScreen.renderer.enabled = true;
+			Time.timeScale = 0.0f;
+		}
+	}
+}
